ce4ef11fa9d087d0ca15f6ff859d6d7d28a849ae
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-02-10 11:26:46 -0600

    working on read file method

--- src/PracticeFileSystem.java
+++ src/PracticeFileSystem.java
@@ -1,12 +1,18 @@
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.*;
+import java.util.*;
 
 public class PracticeFileSystem {
 
     public static void main (String [] args) {
+
         newFile();
+        writeFile();
+//        readFile();
+//        readWriteFile();
+
     } // main class end
     static void newFile() {
 
@@ -27,9 +33,51 @@ public class PracticeFileSystem {
             }
         } catch (IOException ioe) {
             ioe.printStackTrace();
-        } // try - catch end
+        } // try - catch block
 
     } // new file end
 
+    static void writeFile() {
+        List<String> fruit = new ArrayList<>();
+        fruit.add("banana"); fruit.add("orange"); fruit.add("peach"); fruit.add("pineapple");
+        try {
+            Path groceries = Paths.get("data", "groceries.txt");
+            Files.write(groceries, fruit);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } // try-catch block
+
+    } // write file end
+
+    static void readFile() {
+
+//        try {
+//            Path poemFile = Paths.get("europa", "poem.txt");
+//            List<String> poemLines = Files.readAllLines(poemFile);
+//
+//        }
+
+    }
+
+//    static void readWriteFile() {
+//        List <String> fruit = null;
+//        try {
+//            Path groceries = Paths.get("data", "groceries.txt");
+//            fruit Files.readAllLines(groceries);
+//            for (String line : fruit) {
+//                System.out.println(line);
+//            }
+//        } catch (IOException ioe){
+//            ioe.printStackTrace();
+//        }
+//
+//        fruit.add("kiwi"); fruit.add("star fruit");
+//        try {
+//            Path.groceries = Paths.get("data", "groceries.txt");
+//            Files.write(groceries, fruit);
+//        } catch (IOException ioe) {
+//            ioe.printStackTrace();
+//        }
+//    }
 
 } // public class end
